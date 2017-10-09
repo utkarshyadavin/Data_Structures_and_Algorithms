2c57a245a4a5a10eee5e55c2f8e45bd3efc826b3
utkarshyadavin <utkarshyadavin@gmail.com>
2017-10-10 01:29:59 +0530

    added methods to insert a new node at the end and to print the linked list

--- Linked_List/linked_list_implementation.cpp
+++ Linked_List/linked_list_implementation.cpp
@@ -13,9 +13,6 @@ chainNode* next ;
 
 // method to create a new node.
 //Returns the pointer to the created new node.
-
-
-
 chainNode* createNode(int data) {
 chainNode* temp = new chainNode() ; 
 temp->data = data ; 
@@ -25,14 +22,40 @@ return temp ;
 
 
 
+//Method to insert new node into the linked list
+chainNode* insert(chainNode* head , int data){
+if(head ==NULL)
+head = createNode( data) ; 
+else {
+head->next = insert(head->next , data) ; 
+}
+return head; 
+}
+
+
+//Method to print the linked list
+void printLinkedList(chainNode* head) {
+if(head==NULL){
+cout<<"The linked list is empty"<<endl ;
+return ;}
+else {
+while(head!=NULL){
+cout<<head->data<<" ";
+head = head->next ; 
+}
+}
+}
+
+
+
+
+//main method
 
 int main () {
-	
 chainNode* head = NULL ; 
-head = createNode(10) ; 
+head = insert(head , 10 ) ; 
+head = insert(head , 11 ) ;
+head = insert(head , 12 ) ;
+printLinkedList(head) ;
 
-	
-	
-	
-	
 }
