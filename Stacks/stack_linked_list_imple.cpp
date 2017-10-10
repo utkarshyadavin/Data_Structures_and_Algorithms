960288c1ea89a0fdd3c58e1d7ea12e7d5903eb86
utkarshyadavin <utkarshyadavin@gmail.com>
2017-10-10 17:58:33 +0530

    Added the stack implementation using a linked list

---
+++ Stacks/stack_linked_list_imple.cpp
@@ -0,0 +1,78 @@
+//Implementing stack using a linked list
+
+#include <iostream>
+using namespace std;
+
+
+//defining the structure of a stacknode.
+struct stackNode {
+int data ; 
+stackNode* next ; 
+};
+
+
+// creating a new stacknode with the given data
+stackNode* newNode(int data){
+stackNode* temp = new stackNode() ;
+temp->data = data ; 
+temp->next = NULL ; 
+return temp ;
+}
+
+
+//Checking whether the stack is empty or not
+bool isEmpty(stackNode* root){
+if(root==NULL)
+return 1 ; 
+else 
+return 0 ; 
+}
+
+
+//Pushing a new element on top of the stack
+void push( stackNode** root , int data){
+stackNode* stackNode = newNode(data) ; 
+stackNode->next = *root ; 
+*root = stackNode ; 
+}
+
+
+//Popping an element from top of the stack.
+int pop(stackNode** root , int data){
+if (isEmpty(*root)){
+cout<<"The stack is empty"<<"\n"  ; 
+return -1; 
+}
+else{
+stackNode* temp = *root ; 
+*root = (*root)->next ; 
+int popped = temp->data ; 
+delete(temp) ; 
+return popped ; 
+}
+}
+
+int top(stackNode* root){
+	
+	
+}
+void printStack(stackNode* root){
+if(root ==NULL)
+cout<<"The stack is empty"<<" \n" ; 
+while (root!=NULL){
+cout<<root->data<<" " ; 
+root= root->next ; 
+}
+}
+
+
+int main() {
+	
+	stackNode* root = NULL ; 
+	push(&root , 10) ; 
+	push(&root , 15) ; 
+	push(&root , 16) ; 
+	printStack(root) ; 
+	
+	
+}
